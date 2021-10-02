3b02b0fac4926a0a9c57adcc524c4407848ce4f7
LENOVO <felixyehdeya@gmail.com>
2021-10-02 15:01:31 +0700

    Ini Code

---
+++ app/src/main/java/com/example/uts_pbp_b_kelompok_1/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.uts_pbp_b_kelompok_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textLink = findViewById(R.id.textLink);
+
+        textLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
