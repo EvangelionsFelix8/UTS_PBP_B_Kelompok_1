ebbf2fb4c71c55473b42888402a87e15ef8229d1
chrliem <christian.d.setiawan@gmail.com>
2021-10-16 20:50:26 +0700

    Update Chr 16/10/2021 20.50

--- app/src/main/java/com/example/uts_pbp_b_kelompok_1/Fragment/FragmentHome.java
+++ app/src/main/java/com/example/uts_pbp_b_kelompok_1/Fragment/FragmentHome.java
@@ -9,6 +9,7 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,14 +34,17 @@ public class FragmentHome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        //return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding = DataBindingUtil.setContentView((Activity) this.getContext(), R.layout.fragment_home);
+        //binding = DataBindingUtil.setContentView((Activity) this.getContext(), R.layout.fragment_home);
+
         listEvent = new DummyEvent().dataEvent;
 
         EventAdapter adapter = new EventAdapter(listEvent);
