bf00e1e30d39ad798e2fa3e7ce09c6fd035aa835
chrliem <christian.d.setiawan@gmail.com>
2021-10-19 14:18:05 +0700

    Update Chr 19/10/2021 14.20PM

--- app/src/main/java/com/example/uts_pbp_b_kelompok_1/DetailEventActivity.java
+++ app/src/main/java/com/example/uts_pbp_b_kelompok_1/DetailEventActivity.java
@@ -1,8 +1,18 @@
 package com.example.uts_pbp_b_kelompok_1;
 
+import static com.mapbox.core.constants.Constants.PRECISION_6;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineCap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineJoin;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
+
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -17,7 +27,16 @@ import com.google.android.material.button.MaterialButton;
 import com.google.gson.Gson;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.MapboxDirections;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
 import com.mapbox.mapboxsdk.location.LocationComponentOptions;
@@ -27,9 +46,18 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.layers.Property;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.mapboxsdk.utils.BitmapUtils;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
 
 public class DetailEventActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
 
@@ -39,7 +67,16 @@ public class DetailEventActivity extends AppCompatActivity implements OnMapReady
     private PermissionsManager permissionsManager;
     private MapboxMap mapboxMap;
     private MapView mapView;
-
+    private Point origin;
+    private Point destination;
+    private LatLng point;
+    private MapboxDirections client;
+    private static final String ROUTE_LAYER_ID = "route-layer-id";
+    private static final String ROUTE_SOURCE_ID = "route-source-id";
+    private static final String ICON_LAYER_ID = "icon-layer-id";
+    private static final String ICON_SOURCE_ID = "icon-source-id";
+    private static final String RED_PIN_ICON_ID = "red-pin-icon-id";
+    private DirectionsRoute currentRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -117,11 +154,120 @@ public class DetailEventActivity extends AppCompatActivity implements OnMapReady
         mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
+
                 enableLocationComponent(style);
+                //origin = Point.fromLngLat(point.getLongitude(), point.getLatitude());
+                //destination = Point.fromLngLat(getVenueLongitude(),getVenueLatitude());
+                //initSource(style);
+                //initLayer(style);
+
+                //getRoute(mapboxMap, origin, destination);
             }
         });
 
     }
+
+    /*private void initSource(Style style) {
+        style.addSource(new GeoJsonSource(ROUTE_SOURCE_ID));
+
+        GeoJsonSource iconGeoJsonSource = new GeoJsonSource(ICON_SOURCE_ID, FeatureCollection.fromFeatures(new Feature[] {
+                Feature.fromGeometry(Point.fromLngLat(origin.longitude(), origin.latitude())),
+                Feature.fromGeometry(Point.fromLngLat(destination.longitude(), destination.latitude()))}));
+        style.addSource(iconGeoJsonSource);
+    }
+
+    private void initLayer(Style style){
+        LineLayer routeLayer = new LineLayer(ROUTE_LAYER_ID, ROUTE_SOURCE_ID);
+
+        routeLayer.setProperties(
+                lineCap(Property.LINE_CAP_ROUND),
+                lineJoin(Property.LINE_JOIN_ROUND),
+                lineWidth(5f),
+                lineColor(Color.parseColor("#009688")));
+
+        style.addLayer(routeLayer);
+
+// Add the red marker icon image to the map
+        style.addImage(RED_PIN_ICON_ID, BitmapUtils.getBitmapFromDrawable(
+                getResources().getDrawable(R.drawable.red_marker)));
+
+// Add the red marker icon SymbolLayer to the map
+        style.addLayer(new SymbolLayer(ICON_LAYER_ID, ICON_SOURCE_ID).withProperties(
+                iconImage(RED_PIN_ICON_ID),
+                iconIgnorePlacement(true),
+                iconAllowOverlap(true),
+                iconOffset(new Float[] {0f, -9f})));
+    }
+
+    private void getRoute(MapboxMap mapboxMap, Point origin, Point destination){
+        client = MapboxDirections.builder()
+                .origin(origin)
+                .destination(destination)
+                .overview(DirectionsCriteria.OVERVIEW_FULL)
+                .profile(DirectionsCriteria.PROFILE_DRIVING)
+                .accessToken(getString(R.string.mapbox_access_token))
+                .build();
+
+        client.enqueueCall(new Callback<DirectionsResponse>() {
+            @Override
+            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                Timber.d("Response code: " + response.code());
+                if (response.body() == null) {
+                    Timber.e("No routes found, make sure you set the right user and access token.");
+                    return;
+                } else if (response.body().routes().size() < 1) {
+                    Timber.e("No routes found");
+                    return;
+                }
+
+                currentRoute = response.body().routes().get(0);
+
+                if (mapboxMap != null) {
+                    mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                        @Override
+                        public void onStyleLoaded(@NonNull Style style) {
+                            GeoJsonSource source = style.getSourceAs(ROUTE_SOURCE_ID);
+
+                            if (source != null) {
+                                source.setGeoJson(LineString.fromPolyline(currentRoute.geometry(), PRECISION_6));
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                Timber.e("Error: " + t.getMessage());
+                Toast.makeText(DetailEventActivity.this, "Error: " + t.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private double getVenueLatitude() {
+        if(event.getVenueEvent().toString().equals("Graha Saba Buana")){
+            return 110.8063;
+        }else if(event.getVenueEvent().toString().equals("GOR Sriwedari")){
+            return 110.8118;
+        }else if(event.getVenueEvent().toString().equals("GOR Sritex Arena")) {
+            return 110.8150;
+        }else if(event.getVenueEvent().toString().equals("Balai Kota Surakarta")){
+            return 110.8293;
+        }else return 0;
+    }
+
+    private double getVenueLongitude() {
+        if(event.getVenueEvent().toString().equals("Graha Saba Buana")){
+            return 7.5447;
+        }else if(event.getVenueEvent().toString().equals("GOR Sriwedari")){
+            return 7.5682;
+        }else if(event.getVenueEvent().toString().equals("GOR Sritex Arena")) {
+            return 7.5710;
+        }else if(event.getVenueEvent().toString().equals("Balai Kota Surakarta")){
+            return 7.5695;
+        }else return 0;
+    } */
 
     @SuppressWarnings( {"MissingPermission"})
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
